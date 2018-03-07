edb3b456e04ba0cf9bf1eb27dd715a9d8d1519c6
John Doe <ali85.tech@gmail.com>
2018-03-07 20:33:24 +0200

    Entities

---
+++ main.py
@@ -0,0 +1,8 @@
+import models
+
+member1=models.Member("Ahmad",22)
+member2 =models.Member("Fadi",30)
+
+post1=models.Post("1st Title","Content of the first post")
+post2=models.Post("second Title","Content of the second post")
+post3=models.Post("Third Title","Content of the third post")
